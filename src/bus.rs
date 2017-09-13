be8a0e50a86a1d8bf6bd69aadf7c19a5c93ef64e
Ryan Leckey <leckey.ryan@gmail.com>
2017-09-13 05:52:03 -0700

    Add 16-bit helpers to Bus

--- src/bus.rs
+++ src/bus.rs
@@ -1,4 +1,18 @@
 pub trait Bus {
     fn read8(&self, address: u16) -> u8;
     fn write8(&mut self, address: u16, value: u8);
+
+    #[inline]
+    fn read16(&self, address: u16) -> u16 {
+        let l = self.read8(address);
+        let h = self.read8(address.wrapping_add(1));
+
+        ((h as u16) << 8) | (l as u16)
+    }
+
+    #[inline]
+    fn write16(&mut self, address: u16, value: u16) {
+        self.write8(address.wrapping_add(1), (value >> 8) as u8);
+        self.write8(address, value as u8);
+    }
 }
