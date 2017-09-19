e3fe535c2a49e26865dd86ab7ec8ffd02eae342c
Ryan Leckey <leckey.ryan@gmail.com>
2017-09-19 15:35:15 -0700

    :memo:

--- src/bus.rs
+++ src/bus.rs
@@ -1,16 +1,22 @@
+/// Describes a system "bus" that is intended to be implemented on its corresponding component.
+///
+/// A tuple of `Bus` can be used as an "Interconnect".
 pub trait Bus {
     /// Returns `true` if this instance can handle the given `address`.
+    #[inline]
     fn contains(&self, _: u16) -> bool {
         false
     }
 
     /// Returns the 8-bit value corresponding to the `address`.
+    #[inline]
     fn read8(&self, _: u16) -> u8 {
         // Return an "unconnected" value
         0xff
     }
 
     /// Writes the 8-bit value to the given `address`.
+    #[inline]
     fn write8(&mut self, _: u16, _: u8) {
         // Do nothing
     }
@@ -43,6 +49,7 @@ macro_rules! impl_bus_tuple {
             }
 
             #[allow(non_snake_case)]
+            #[inline]
             fn read8(&self, address: u16) -> u8 {
                 let ($(ref $name,)*) = *self;
                 $(if $name.contains(address) { return $name.read8(address); })*
@@ -53,6 +60,7 @@ macro_rules! impl_bus_tuple {
             }
 
             #[allow(non_snake_case)]
+            #[inline]
             fn write8(&mut self, address: u16, value: u8) {
                 let ($(ref mut $name,)*) = *self;
                 $(if $name.contains(address) { return $name.write8(address, value); })*
