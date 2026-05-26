5bf61e8948a2b71bf12914b53a6542b15f06cc8d
Ryan Leckey <leckey.ryan@gmail.com>
2026-05-25 17:24:58 -0700

    feat: wire up CLI entry point with ROM open and run loop
    Signed-off-by: Ryan Leckey <leckey.ryan@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
 // Copyright (C) 2026 Ryan Leckey <leckey.ryan@gmail.com>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-fn main() {}
+use std::path::PathBuf;
+
+use clap::Parser;
+use wadatsumi::System;
+
+#[derive(Parser)]
+struct Args {
+    pak: PathBuf,
+}
+
+fn main() -> wadatsumi::Result<()> {
+    let args = Args::parse();
+
+    let mut system = System::new();
+
+    system.open(&args.pak)?;
+
+    loop {
+        system.tick()?;
+    }
+}
